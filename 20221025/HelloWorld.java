9ecb1adfef188c817d4910868b2553c97393cb83
luzhiminhaoshuai <2262991710@qq.com>
2023-01-05 11:07:39 +0800

    这是一个开始学习java的代码

---
+++ 20221025/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA
+ * Description:
+ * User:luzhimin
+ * Date:2023-01-04
+ * Time:11:17
+ */
+public class HelloWorld {
+    //m main psvm
+    public static void main(String[] args){
+       //sout 出现提示后点回车即可
+        System.out.println("hello world");
+        int a=10;
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+    }
+}
